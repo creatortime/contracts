5fc4e98c045360b630785d76e82149f117f6008c
creatortime <imaster56@mail.ru>
2025-12-19 22:58:57 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
     version: "0.8.19",
@@ -13,16 +12,12 @@ module.exports = {
     }
   },
   networks: {
-    hardhat: {
-      chainId: 31337
-    },
     localhost: {
-      url: "http://127.0.0.1:8545",
-      chainId: 31337
+      url: "http://127.0.0.1:8545"
     },
     mumbai: {
-      url: process.env.MUMBAI_RPC_URL || "https://rpc-mumbai.maticvigil.com",
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : []
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: []
     }
   }
 };
