97fa9d9703ee07a05c2bed04148366924f4d0a78
creatortime <imaster56@mail.ru>
2025-12-19 18:05:33 +0500

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,15 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with account:", deployer.address);
 
+  const CreatorToken = await ethers.getContractFactory("CreatorToken");
+  const token = await CreatorToken.deploy();
+
+  await token.deployed();
+  console.log("CreatorToken deployed to:", token.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
