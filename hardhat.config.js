81e8866f76b30beb186a25d75680d3f140d6fd8e
creatortime <imaster56@mail.ru>
2025-12-22 15:37:45 +0500

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,10 +11,15 @@ module.exports = {
       }
     }
   },
- networks: {
-  sepolia: {
-    url: "https://ethereum-sepolia.publicnode.com", // Надёжный публичный RPC
-    accounts: [`0x${process.env.PRIVATE_KEY}`]
-  }
+  networks: {
+    // Локальная сеть для очень быстрого тестирования
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    // Новая целевая тестовая сеть - Sepolia
+    sepolia: {
+      url: "https://ethereum-sepolia.publicnode.com", // Надежный бесплатный RPC-узел
+      accounts: [`0x${process.env.PRIVATE_KEY}`] // Ключ берется из .env файла
+    }
   }
 };
